1b878fc72fd50330268d5d87531c513fce83aad8
Tux <ricardo.palazzio@gmail.com>
2019-07-27 09:55:13 -0300

    adicionado join como exemplo

--- auth-server/src/main/java/ninja/tuxtech/api/endpoint/ClienteEndPoint.java
+++ auth-server/src/main/java/ninja/tuxtech/api/endpoint/ClienteEndPoint.java
@@ -1,9 +1,7 @@
 package ninja.tuxtech.api.endpoint;
 
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
-import net.kaczmarzyk.spring.data.jpa.web.annotation.JoinFetch;
-import net.kaczmarzyk.spring.data.jpa.web.annotation.Or;
-import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.*;
 import ninja.tuxtech.api.entity.Cliente;
 import ninja.tuxtech.api.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+@Joins({ @Join( path = "address" , alias = "address")})
 @Or({
         @Spec(path="name", params="name", spec= Like.class),
-        @Spec(path="surname", params="name", spec= Like.class)
+        @Spec(path="surname", params="name", spec= Like.class),
+        @Spec(path = "address.street" , params="address", spec= Like.class )
 })
+@JoinFetch(paths = { "address" })
 interface ClienteSpec extends Specification<Cliente> {
 }
 
@@ -40,9 +41,7 @@ public class ClienteEndPoint extends CrudEndPoint<Cliente, Long>{
 
 
     @GetMapping("/search")
-    public ResponseEntity<?> all( @JoinFetch(paths = { "address" }) ClienteSpec spec , Pageable pageable){
+    public ResponseEntity<?> all( ClienteSpec spec , Pageable pageable){
         return ResponseEntity.ok(repository.findAll(spec , pageable));
     }
 }
-
-//@JoinFetch(paths = { "address" })
